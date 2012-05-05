82b2e08f8ace01cfb2c63b6234fd3c6bc796bdfd
Takashi Masuyama <mamewotoko@gmail.com>
2012-05-05 22:39:00 +0900

    add build.xml by updating project as test project

---
+++ lib/setup.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+wget 'http://code.google.com/p/robotium/downloads/detail?name=robotium-solo-3.2.1.jar&can=2&q=' -O robotium-solo-2.3.1.jar
+
---
+++ run.sh
@@ -0,0 +1,1 @@
+adb shell am instrument -w /sdcard/malarm.ec com.mamewo.malarm_test/android.test.InstrumentationTestRunner 
