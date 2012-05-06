0f7716eddf25b0dcd49d4af8bd9b54daccb7da33
Takashi Masuyama <mamewotoko@gmail.com>
2012-05-06 10:30:04 +0900

    add notest mode

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools
 AVD=XperiaEmu
 PORT=5554
+TARGET=installd
+echo arg $1
+if [ "$1" != "notest" ]; then
+  TARGET="$TARGET test"
+fi
 
 adb devices | grep emulator-$PORT > /dev/null
 if [ $? -ne 0 ]; then
@@ -13,7 +18,8 @@ fi
 
 echo start testing
 # unlock screen by pressing menu key
+adb shell input keyevent 3 
 adb shell input keyevent 82
 ant debug
-ant installd test
+ant $TARGET
 
